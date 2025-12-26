7a406fa5882993850aa03ab0902c81c0c531b01b
InspectorIT <sshipunovv008@gmail.com>
2025-12-26 17:04:17 +0300

    Upgrade tests

--- python-fermat/test_fermat.py
+++ python-fermat/test_fermat.py
@@ -4,9 +4,9 @@ import fermat
 class Tests(unittest.TestCase):
     def test_n_3(self):
         self.assertEqual(fermat.fermat_holds(3), True)
-    def test_n_4():
+    def test_n_4(self):
         self.assertEqual(fermat.fermat_holds(4), True)
-    def test_n_2():
+    def test_n_2(self):
         self.assertEqual(fermat.fermat_holds(2), False)
 
 if __name__ == "__main__":
